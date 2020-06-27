1a8364aded17aa01f157339e2f3fd4590f42584f
noranmuri <62883338+noranmuri@users.noreply.github.com>
2020-06-27 21:19:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 
 from konlpy.tag import Twitter
 from collections import Counter
+import copy
 import os
+import time
 import re
 import pandas as pd
 import shutil
@@ -38,38 +40,54 @@ def urlUploadDef():
 	if request.method == 'POST':
 		inputurl = request.form['url']
 		str = inputurl
-		#url = str		
+		#url = str
 		res = requests.get(str)
-		html = BeautifulSoup(res.content, "html.parser")	
+		html = BeautifulSoup(res.content, "html.parser")
 		return render_template('infoPage.html', parsed_page=html)
 
 # 단어 수 체크하여 튜플 리스트로 반환
-def countNoun(lines):
-	List = list()
-	l = len(lines)
-	for i in range(0,l):
-			str = lines[i]										# <--- str 예쁜 모양으로 만들어야 함
-			res = requests.get(str)
-			html = BeautifulSoup(res.content, "html.parser")
-			tmp = html.get_text()	# tmp는 문자열로만 이루어진 정돈된 문자열 리스트
-			#nouns = nlpy.nouns(tmp)
-			nouns = tmp.split()									# <--- 단어 체크하는 부분 확실하게
-			count = Counter(nouns) # 단어와 빈도수의 튜플로 이루어진 리스트
-			List.append((str,len(count)))
-	return List
-	
+def countNoun(e, list):
+	start = time.time()
+	res = requests.get(e[1])
+	html = BeautifulSoup(res.content, "html.parser")
+	tmp = html.get_text()	# tmp는 문자열로만 이루어진 정돈된 문자열 리스트
+	nouns = tmp.split()									# <--- 단어 체크하는 부분 확실하게
+	count = Counter(nouns) # 단어와 빈도수의 튜플로 이루어진 리스트
+	run = time.time()-start
+	list[e[0]] = (e[0], e[1], len(count), run)
+
+@app.route('/repeatPage', methods = ['POST'])
+def repeatPage():
+	if request.method == 'POST':
+		index = request.form['index']
+		url = request.form['url']
+		List = request.form['list']
+		#return render_template('example.html',index = index, url = url, list = List)			
+		List = [] + countNoun(List[index], List)
+		return render_template('ex.html',list = List)
+		#return render_template('table.html',list = List)
 
 # 입력받은 파일을 크롤링하여 표가 있는 페이지로 연결
 @app.route('/fileUploadDef', methods = ['POST'])
 def fileUploadDef():
-	if request.method == 'POST':	
+	if request.method == 'POST':
 		file = request.files['file']
 		#file = open(f.filename, 'r')
 		lines = file.readlines()
 		l = len(lines)
-		List = countNoun(lines)
-		#nlpy = Twitter()
-		return render_template('table.html', list = List)
+		List = list()					# List ( index, url, noun count, time )
+		for i in range(0,l):
+			start = time.time()
+			lines[i] = lines[i].decode('ascii')
+			res = requests.get(lines[i])
+			html = BeautifulSoup(res.content, "html.parser")
+			tmp = html.get_text()	# tmp는 문자열로만 이루어진 정돈된 문자열 리스트
+			nouns = tmp.split()							# <--- 단어 체크하는 부분 확실하게
+			count = Counter(nouns) # 단어와 빈도수의 튜플로 이루어진 리스트
+			run = time.time()-start
+			List.append((i,lines[i],len(count),run))
+		#return render_template('example.html', list = List)
+		return render_template('table.html', list = List)	
 
 # main
 if __name__ == '__main__':

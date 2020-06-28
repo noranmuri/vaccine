8058f9a9ac725b923a0b57bcf04c3c5ef5abbc32
noranmuri <62883338+noranmuri@users.noreply.github.com>
2020-06-28 21:02:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,46 +44,58 @@ def count(line):
 		
 	noun = tmp.split()
 	c = Counter(noun)
-		
+
 	return len(c)
-			
-	
+
+# 입력된 url을 가지고 크롤링 하여 결과를 나타내는 페이지로 연결
 @app.route('/urlUploadDef', methods = ['POST'])
 def urlUploadDef():
 	error = None
 	if request.method == 'POST':
 		start=time.time()
-		
 		inputurl = request.form['url']
 		str = inputurl
 		state = 'success'
-		res = requests.get(str)
+		res = requests.get(inputurl)
 		html = BeautifulSoup(res.content, "html.parser")
-		
 		t = time.time() - start
-		c = count(inputurl)	
-		return render_template('infoPage.html',  c= c,t=t,state=state,inputurl=inputurl)
+		c = count(inputurl)
+		return render_template('infoPage.html',  c=c, t=t, state=state, inputurl=inputurl)
+
+@app.route('/repeatUrl', methods = ['POST'])
+def repeatPageurl():
+	if request.method == 'POST':
+		start = time.time()
+		url = request.form['url']
+		res = requests.get(url)
+		html = BeautifulSoup(res.content, "html.parser")
+		run = time.time() - start
+		c = count(inputurl)
+		state = "change"
+		return render_template('infoPage.html', c=c, t=run, state = state, inputurl=url)
 
 # 단어 수 체크하여 튜플 리스트로 반환
-def countNoun(e, list):
+def countNoun(url):
 	start = time.time()
-	res = requests.get(e[1])
+	res = requests.get(url)
 	html = BeautifulSoup(res.content, "html.parser")
 	tmp = html.get_text()	# tmp는 문자열로만 이루어진 정돈된 문자열 리스트
-	nouns = tmp.split()									# <--- 단어 체크하는 부분 확실하게
-	count = Counter(nouns) # 단어와 빈도수의 튜플로 이루어진 리스트
+	nouns = tmp.split()
+	count = Counter(nouns)  # 단어와 빈도수의 튜플로 이루어진 리스트
 	run = time.time()-start
-	list[e[0]] = (e[0], e[1], len(count), run)
+	return count,run
 
 @app.route('/repeatPage', methods = ['POST'])
 def repeatPage():
 	if request.method == 'POST':
 		index = request.form['index']
-		url = request.form['url']
-		List = request.form['list']
-		#return render_template('example.html',index = index, url = url, list = List)			
-		List = [] + countNoun(List[index], List)
-		return render_template('ex.html',list = List)
+		list = request.form['list']
+		return render_template('ex.html', index = index, list = list)
+		#return render_template('example.html',index = index, url = url, list = List)
+		element = List[index]		
+		url = element['url']	#url
+		count, run = countNoun(url)
+		List[index] = (index, url, count, run)
 		#return render_template('table.html',list = List)
 
 # 입력받은 파일을 크롤링하여 표가 있는 페이지로 연결
@@ -95,19 +107,56 @@ def fileUploadDef():
 		lines = file.readlines()
 		l = len(lines)
 		List = list()					# List ( index, url, noun count, time )
+		state = "success"	
+		
 		for i in range(0,l):
-			start = time.time()
-			lines[i] = lines[i].decode('ascii')
-			res = requests.get(lines[i])
-			html = BeautifulSoup(res.content, "html.parser")
-			tmp = html.get_text()	# tmp는 문자열로만 이루어진 정돈된 문자열 리스트
-			nouns = tmp.split()							# <--- 단어 체크하는 부분 확실하게
-			count = Counter(nouns) # 단어와 빈도수의 튜플로 이루어진 리스트
-			run = time.time()-start
-			List.append((i,lines[i],len(count),run))
-		#return render_template('example.html', list = List)
-		return render_template('table.html', list = List)	
+			if lines[i] not in lines:
+				state = "overlap"
+				List.append(
+					{
+					"index":i,
+					"url":lines[i],
+					"len":0,
+					"words":0,
+					"time":0,
+					"state":state,
+					}			
+				)		
+			else:
+				#spliter = Twitter()				
+				lines[i] = lines[i].decode('ascii')
+				start = time.time()
+				res = requests.get(lines[i])
+				html = BeautifulSoup(res.content, "html.parser")	
+				tmp = html.get_text()	# tmp는 문자열로만 이루어진 정돈된 문자열 리스트
+				nouns = tmp.split()
+				#return render_template('ex.html', url = nouns)
+				count = Counter(nouns)				
+				"""return_list = []
+				for n,c in count.most_common(10):
+					temp = {'tag':n, 'count':c}
+					return_list.append(temp)				
+				#count = Counter(nouns) # 단어와 빈도수의 튜플로 이루어진 리스트"""
+				run = time.time()-start
+				List.append(	
+					{
+						"index":i,
+						"url":lines[i],
+						"len":len(count),
+						"words":count,
+						"time":run,
+						"state":state,
+					}			
+				)
+		
+		return render_template('table.html', list = List)
 
+@app.route('/top10Page', methods = ['POST'])
+def top10Page():
+	list = request.form['list']
+	url = request.form['url']
+	#return render_template('ex.html', url = list)
+	return render_template('top10Page.html', list = list, url = url)
 # main
 if __name__ == '__main__':
-	app.run(debug=True); 
+	app.run(debug=True);
